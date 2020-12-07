ad1390a3f73e8594f1bad909d1e59fdcb406a497
Andrew Lo <andrewclo42@gmail.com>
2020-12-06 20:24:31 -0800

    set up files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Header } from "./global/header";
+import { Switch, Route } from "react-router-dom";
+
+import PlaceOrder from "./main/PlaceOrder";
+import UpdatePredicted from "./main/UpdatePredicted";
+import Kitchen from "./main/Kitchen";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={PlaceOrder} />
+          <Route path="/updatepredicted" component={UpdatePredicted} />
+          <Route path="/kitchen" component={Kitchen} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
