5af1ed2d734276e1f8e2ff0c9938631fe73ed1ac
Andrew Lo <andrewclo42@gmail.com>
2020-12-28 18:34:28 -0800

    added form regex and submit button

--- src/pages/components/addItem.js
+++ src/pages/components/addItem.js
@@ -1,19 +1,30 @@
 import React, { Component } from "react";
 import {Container} from "reactstrap";
+import { socket } from "../../header/header";
 
 class AddItem extends Component {
   constructor() {
     super();
     this.state = {
       name: '',
-      currentQty: ''
+      currentQty: '0'
       // this is where we are connecting to with sockets,
     };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({name: event.target.value})
+    let intReg = /^\d+$/;
+    if(event.target.name === "name"){
+      this.setState({name: event.target.value})
+    } else if(event.target.name === "currentQty" && intReg.test(event.target.value)) {
+      this.setState({currentQty: event.target.value})
+    }
+  }
+
+  handleNewItem(){
+    let predicted_details = this.state;
+    socket.emit("AddMenuItem", predicted_details);
   }
 
   render() {
@@ -22,9 +33,10 @@ class AddItem extends Component {
         <h4 className="text-center">New Menu Item</h4>
         <div className="form-container">
           <label>Item Name</label>
-          <input value={this.state.name} name="name" onChange={this.handleChange}></input>
+          <input value={this.state.name} name="name" onChange={this.handleChange} autoComplete="off"></input>
           <label>Item Quantity</label>
-          <input></input>
+          <input value={this.state.currentQty} name="currentQty" onChange={this.handleChange} autoComplete="off"></input>
+          <button className="submit-button mt-5">Add Item</button>
         </div>
       </Container>
     )
