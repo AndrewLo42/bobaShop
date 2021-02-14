074d6942391ce4b5669aca6f666500463413a86c
Andrew Lo <andrewclo42@gmail.com>
2021-02-13 21:07:17 -0800

    added basic order cards for the kitchen

---
+++ src/pages/components/orderCard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class OrderCard extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return(
+      <div>
+        Order:
+      </div>
+    );
+  }
+}
+
+export default OrderCard;
