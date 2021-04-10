00953f7778722241734c516b65c11954d3a811ba
Andrew Lo <andrewclo42@gmail.com>
2021-04-10 16:39:12 -0700

    added addons to menu, tweak css

--- src/pages/components/addOnModal.js
+++ src/pages/components/addOnModal.js
@@ -12,29 +12,33 @@ class AddOns extends Component {
 
   render() {
     return(
-      <div>
-        <h4>Drink Options</h4>
-        <div><b>Sweetness</b></div>
-        <div>
-          <div className={`select-button ${this.state.sweetness === 100 ? option-selected : option-unselected}`} onClick={()=>this.setState({sweetness: 100})}>100%</div>
-          <div className={`select-button ${this.state.sweetness === 50 ? option-selected : option-unselected}`} onClick={()=>this.setState({sweetness: 50})}>50%</div>
-          <div className={`select-button ${this.state.sweetness === 25 ? option-selected : option-unselected}`} onClick={()=>this.setState({sweetness: 25})}>25%</div>
-          <div className={`select-button ${this.state.sweetness === 0 ? option-selected : option-unselected}`} onClick={()=>this.setState({sweetness: 0})}>0%</div>
+      <div className="add-modal" >
+        <div className="modal-dialog" >
+          <h4 className="modal-title">Drink Options</h4>
+          <div><b>Sweetness</b></div>
+          <div>
+            <div className={`select-button ${this.state.sweetness === 100 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({sweetness: 100})}>100%</div>
+            <div className={`select-button ${this.state.sweetness === 50 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({sweetness: 50})}>50%</div>
+            <div className={`select-button ${this.state.sweetness === 25 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({sweetness: 25})}>25%</div>
+            <div className={`select-button ${this.state.sweetness === 0 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({sweetness: 0})}>0%</div>
+          </div>
+          <div><b>Boba</b></div>
+          <div>
+            <div className={`select-button ${this.state.boba === true ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({boba: true})}>Yes</div>
+            <div className={`select-button ${this.state.boba === false ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({boba: false})}>No</div>
+          </div>
+          <div><b>Ice Levels</b></div>
+          <div>
+            <div className={`select-button ${this.state.ice === 100 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({ice: 100})}>100%</div>
+            <div className={`select-button ${this.state.ice === 50 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({ice: 50})}>50%</div>
+            <div className={`select-button ${this.state.ice === 25 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({ice: 25})}>25%</div>
+            <div className={`select-button ${this.state.ice === 0 ? 'option-selected' : 'option-unselected'}`} onClick={()=>this.setState({ice: 0})}>0%</div>
+          </div>
+          <button>Add to Order</button>
         </div>
-        <div><b>Boba</b></div>
-        <div>
-          <div className={`select-button ${this.state.boba === true ? option-selected : option-unselected}`} onClick={()=>this.setState({boba: true})}>Yes</div>
-          <div className={`select-button ${this.state.boba === false ? option-selected : option-unselected}`} onClick={()=>this.setState({boba: false})}>No</div>
-        </div>
-        <div><b>Ice Levels</b></div>
-        <div>
-          <div className={`select-button ${this.state.ice === 100 ? option-selected : option-unselected}`} onClick={()=>this.setState({ice: 100})}>100%</div>
-          <div className={`select-button ${this.state.ice === 50 ? option-selected : option-unselected}`} onClick={()=>this.setState({ice: 50})}>50%</div>
-          <div className={`select-button ${this.state.ice === 25 ? option-selected : option-unselected}`} onClick={()=>this.setState({ice: 25})}>25%</div>
-          <div className={`select-button ${this.state.ice === 0 ? option-selected : option-unselected}`} onClick={()=>this.setState({ice: 0})}>0%</div>
-        </div>
-        <div>Add to Order</div>
       </div>
     )
   }
 }
+
+export default AddOns;
